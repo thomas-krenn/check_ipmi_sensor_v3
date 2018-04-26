c25a0788d7b0d06ebc030244bc8f9abc0f4169f5
Trevor.Max <Trevor.Max@btg.co.nz>
2018-04-26 15:15:53 +1200

    fix_1: add id name type and units to results

--- ipmi/format_func.py
+++ ipmi/format_func.py
@@ -31,7 +31,8 @@ def format_sel_result(doc):
     if sel_issues_present == 1:
         ret_doc = "1 system event log (SEL) entry present"
     elif sel_issues_present > 1:
-        ret_doc = "{} system event log (SEL) entries present".format(sel_issues_present)
+        ret_doc = "{} system event log (SEL) entries present".format(
+            sel_issues_present)
 
     return "IPMI Status: Critical [{}]".format(ret_doc)
 
@@ -39,14 +40,14 @@ def format_sel_result(doc):
 def format_ipmi_sensor_result(doc):
     """
     translate the header and extract the doc into dict
-    ordering: reading, lower nc, upper nc, lower c, upper c 
+    ordering: reading, lower nc, upper nc, lower c, upper c
     """
     doc_by_row = doc.split("\n")
     header = doc_by_row[0]
     body = doc_by_row[1:]
 
     origin_header_list = map(lambda i: i.strip(), header.split("|"))
-    header_list = [HDRMAP.get(header) for header in origin_header_list]
+    header_list = [HDRMAP.get(h) for h in origin_header_list]
 
     result_dict = []
     for row in body:
@@ -62,12 +63,12 @@ def format_ipmi_sensor_result(doc):
 
     return " ".join([
         "'{name}'={reading};{lowerNC}:{upperNC};{lowerNR}:{upperNR}".format(
-            name = row["name"],
-            reading = row["reading"],
-            lowerNC = row["lowerNC"],
-            upperNC = row["upperNC"],
-            lowerNR = row["lowerNR"],
-            upperNR = row["upperNR"],
+            name=row["name"],
+            reading=row["reading"],
+            lowerNC=row["lowerNC"],
+            upperNC=row["upperNC"],
+            lowerNR=row["lowerNR"],
+            upperNR=row["upperNR"],
         ) for row in result_dict
     ])
 
@@ -89,14 +90,14 @@ def format_fru_result(doc):
 def ipmi_sensor_netxms_format(doc, filter_thresholds=False):
     """
     translate the header and extract the doc into dict
-    ordering: reading, lower nc, upper nc, lower c, upper c 
+    ordering: reading, lower nc, upper nc, lower c, upper c
     """
     doc_by_row = doc.split("\n")
     header = doc_by_row[0]
     body = doc_by_row[1:]
 
     origin_header_list = map(lambda i: i.strip(), header.split("|"))
-    header_list = [HDRMAP.get(header) for header in origin_header_list]
+    header_list = [HDRMAP.get(h) for h in origin_header_list]
 
     result_dict = []
     for row in body:
@@ -111,24 +112,22 @@ def ipmi_sensor_netxms_format(doc, filter_thresholds=False):
         # if row_ret["reading"] != "N/A":
         result_dict.append(row_ret)
 
-    header = "Name|Status|Reading|Low NR|LowCT|Low NC|High NC|High CT|High NR"
+    header = "ID|Name|Type|Status|Reading|Unit|Low NR|LowCT|Low NC|High NC|High CT|High NR"
     if filter_thresholds:
-        header = "Name|Status|Reading"
+        header = "ID|Name|Type|Status|Reading|Unit"
 
     body = []
     for row in result_dict:
         if filter_thresholds:
             body_line = "|".join([
-                row["name"], row["state"],
-                "{} {}".format(row["reading"], row["units"]),
+                row["id"], row["name"], row["type"], row["state"],
+                row["reading"], row["units"],
             ])
         else:
             body_line = "|".join([
-                row["name"], row["state"],
-                "{} {}".format(row["reading"], row["units"]),
-                row["lowerNR"], row["lowerC"],
-                row["lowerNC"], row["upperNC"],
-                row["upperC"], row["upperNR"],
+                row["id"], row["name"], row["type"], row["state"],
+                row["reading"], row["units"],
+                row["lowerC"], row["lowerNC"], row["upperNC"], row["upperC"],
             ])
         body.append(body_line)
 
