fc72914979337356385bb0d7dfc9c9c8b909c101
Trevor.Max <Trevor.Max@btg.co.nz>
2018-04-30 09:43:58 +1200

    fix_1: release a new version of ipmi_tool

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ipmi",
-    version="0.5",
+    version="0.6",
     description="a ipmi python client migrate from perl",
     url="https://github.com/zhao-ji/check_ipmi_sensor_v3",
     keywords='ipmi v3',
